42171961423885509b2ff66a3fe2479a6886ac3b
notdule <148584497+notdule@users.noreply.github.com>
2024-05-17 22:44:00 +0200

    Shopping Cart Class

--- script.js
+++ script.js
@@ -100,3 +100,15 @@ products.forEach(
     `;
   }
 );
+
+class ShoppingCart {
+  constructor() {
+    this.items = [];
+    this.total = 0;
+    this.taxRate = 8.25;
+  }
+  
+  addItem(id, products) {
+
+  }
+};
