032e75adda28ab80cd710de315554853e31ab219
notdule <148584497+notdule@users.noreply.github.com>
2024-05-23 23:22:30 +0200

    Adding to Cart functionality

--- script.js
+++ script.js
@@ -136,3 +136,15 @@ class ShoppingCart {
 
 const cart = new ShoppingCart();
 const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
+
+[...addToCartBtns].forEach(
+  (btn) => {
+    btn.addEventListener("click", (event) => {
+      cart.addItem(Number(event.target.id), products);
+    })
+  }
+);
+
+cartBtn.addEventListener("click", () => {
+
+});
