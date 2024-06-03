10166d703ff0cd7480e94cc7b7a61bee90f218f1
notdule <148584497+notdule@users.noreply.github.com>
2024-06-03 21:56:34 +0200

    Update script.js

--- script.js
+++ script.js
@@ -137,16 +137,31 @@ class ShoppingCart {
     return this.items.length;
   }
 
-  calculateTaxes(amount) {
+clearCart() {
+    if (!this.items.length) {
+      alert("Your shopping cart is already empty");
+      return;
+    }
+
+    const isCartCleared = confirm(
+      "Are you sure you want to clear all items from your shopping cart?"
+    );
+
+
+  }
+
+calculateTaxes(amount) {
     return parseFloat(((this.taxRate / 100) * amount).toFixed(2));
   }
 
-calculateTotal() {
+  calculateTotal() {
     const subTotal = this.items.reduce((total, item) => total + item.price, 0);
     const tax = this.calculateTaxes(subTotal);
     this.total = subTotal + tax;
     cartSubTotal.textContent = `$${subTotal.toFixed(2)}`;
-    
+    cartTaxes.textContent = `$${tax.toFixed(2)}`;
+    cartTotal.textContent = `$${this.total.toFixed(2)}`;
+    return this.total;
   }
 };
 
@@ -158,6 +173,7 @@ const addToCartBtns = document.getElementsByClassName("add-to-cart-btn");
     btn.addEventListener("click", (event) => {
       cart.addItem(Number(event.target.id), products);
       totalNumberOfItems.textContent = cart.getCounts();
+      cart.calculateTotal();
     })
   }
 );
